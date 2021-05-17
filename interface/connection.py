c59a9164ad4d33c73b2221267dc7f8a6cbb6fbf6
shadowxinxx <28948400+PyXiaoxin@users.noreply.github.com>
2021-05-17 09:41:11 +0800

    Update connection.py

--- interface/connection.py
+++ interface/connection.py
@@ -1,1 +1,15 @@
-11
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os, sys, re
+import msvcrt
+import paramiko
+import time, threading
+import ftplib
+import telnetlib
+import pprint
+import pymysql
+import logging
+from openpyxl import load_workbook
+from openpyxl import Workbook
+from openpyxl.styles import Font, colors
